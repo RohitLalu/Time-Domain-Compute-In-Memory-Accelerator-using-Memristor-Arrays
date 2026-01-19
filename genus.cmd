f710461895b55a42f663e44c2d6e4db52c041881
Rohit <rohit@ece.nitk.edu.in>
2026-01-19 11:14:26 +0530

    mem array testing read

---
+++ genus.cmd
@@ -0,0 +1,6 @@
+# Cadence Genus(TM) Synthesis Solution, Version 21.14-s082_1, built Jun 23 2022 14:32:08
+
+# Date: Fri Jan 09 11:48:31 2026
+# Host: ece.nitk.edu.in (x86_64 w/Linux 4.18.0-425.3.1.el8.x86_64) (8cores*16cpus*1physical cpu*Intel(R) Xeon(R) W-2245 CPU @ 3.90GHz 16896KB)
+# OS:   Red Hat Enterprise Linux release 8.7 (Ootpa)
+
